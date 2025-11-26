6103b4cf37a6853941ca0823569efa45c8cb37e1
Guillaume Rot <guillaume.rot@gmail.com>
2025-11-26 20:50:35 +0100

    revert

--- prepare_playwright_env.sh
+++ prepare_playwright_env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# install node modules
-npm install
-
 # Install playwright and its dependencies
 npx -y playwright@latest install --with-deps chromium
+
+# install node modules
+npm install
