853356ee4b3d924efdcc3d71a8fa1fc7ab9a2c04
Guillaume Rot <guillaume.rot@gmail.com>
2025-11-26 20:44:38 +0100

    fix: change order to install playwright

--- prepare_playwright_env.sh
+++ prepare_playwright_env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Install playwright and its dependencies
-npx -y playwright@latest install --with-deps chromium
-
 # install node modules
 npm install
+
+# Install playwright and its dependencies
+npx -y playwright@latest install --with-deps chromium
